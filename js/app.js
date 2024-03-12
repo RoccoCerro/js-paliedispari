de0efe6946e6075dee203552047970826b234bb9
RoccoCerro <rcerro02@gmail.com>
2024-03-12 15:04:31 +0100

    ADD ciclo per leggere al contrario la parola inserita dall'utente

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log("ciao")
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma:
+// - la funzione deve ritornare true se la parola è palindroma
+// - deve ritornare false se la parola non è palindroma
+// alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+
+// PSEUDOCODICE
+// Chiedere all’utente di inserire una parola 
+    // Creo una variabile che avrà come valore il contenuto inserito dall'utente tramite prompt
+// Creare una funzione per capire se la parola inserita è palindroma
+    // Creo una variabile stringRevers in cui inserirò la parola scritta al contrario
+    // Confronto la parola inserita dall'utente con stringRevers
+    // Se coincidono faccio tornare true
+    // altrimenti faccio tornare false
+// alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+
+const stringInput = prompt("Inserisci una parola");
+
+function palindrome(string){
+    const stringRevers = "";
+    for(let i = (string.length - 1); i >= 0; --i){
+        stringRevers = string[i];
+    }
+    console.log(stringRevers);
+}
+
+palindrome(stringInput);
+
