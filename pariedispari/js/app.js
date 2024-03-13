bcdde0e18599fa5418c40e92d68bdbc4e682c07e
RoccoCerro <rcerro02@gmail.com>
2024-03-13 09:49:08 +0100

    REFACTOR

--- pariedispari/js/app.js
+++ pariedispari/js/app.js
@@ -7,46 +7,51 @@
 
 // PSEUDOCODICE
 
-const inputNumberUserElement = document.getElementById("input-user");
-const selectEvenOddElement = document.getElementById("select-even-odd")
-const buttonElement = document.getElementById("button")
-const listDomElement = document.querySelector("ul");
-let numberUser = 0;
-let evenOdd = "";
-let numberPC = 0;
+const buttonElement = document.getElementById("button");
 const max = 5;
-let somma = 0;
-let result="";
 
 buttonElement.addEventListener("click", function(){
+    const inputNumberUserElement = document.getElementById("input-user");
+    const selectEvenOddElement = document.getElementById("select-even-odd");
+    const listDomElement = document.querySelector("ul");
+    let numberUser = 0;
+    let numberPC = 0;
+    let evenOdd = "";
+    let somma = 0;
+    let result="";
     numberUser = parseInt(inputNumberUserElement.value);
-    console.log("Numero Utente: ", numberUser)
-    evenOdd = selectEvenOddElement.value;
-    console.log("Hai scelto: ", evenOdd)
-
-    numberPC = functionNumberPc();
-    console.log("Numero PC: ", numberPC)
-
-    somma = numberPC + numberUser;
-    console.log("Somma ",somma);
     
-    console.log(functionEvenOdd(somma))
+    if(isInRange(numberUser) === true){
+        console.log("Numero Utente: ", numberUser)
+        evenOdd = selectEvenOddElement.value;
+        console.log("Hai scelto: ", evenOdd)
     
-    if(evenOdd === functionEvenOdd(somma)){
-        console.log("hai vinto");
-        result = "hai vinto"
+        numberPC = functionNumberPc();
+        console.log("Numero PC: ", numberPC)
+    
+        somma = numberPC + numberUser;
+        console.log("Somma ",somma);
+        
+        console.log(functionEvenOdd(somma))
+        
+        if(evenOdd === functionEvenOdd(somma)){
+            console.log("hai vinto");
+            result = "hai vinto"
+        }else{
+            console.log("hai perso");
+            result = "hai perso"
+        }
+        
+        listDomElement.innerHTML = `
+        <li>Numero Utente: ${numberUser}</li>
+        <li>Hai scelto: ${evenOdd}</li>
+        <li>Numero PC: ${numberPC}</li>
+        <li>Somma: ${somma}</li>
+        <li>Risultato: ${result}</li>
+        `    
     }else{
-        console.log("hai perso");
-        result = "hai perso"
+        alert("Devi inserire un numero compreso tra 1 e 5")
     }
-    
-    listDomElement.innerHTML = `
-    <li>Numero Utente: ${numberUser}</li>
-    <li>Hai scelto: ${evenOdd}</li>
-    <li>Numero PC: ${numberPC}</li>
-    <li>Somma: ${somma}</li>
-    <li>Risultato: ${result}</li>
-    `
 })
 
 function functionNumberPc(){
@@ -62,3 +67,24 @@ function functionEvenOdd(number){
     }
 }
 
+// function control(variabile){
+//     if (variabile > 5 || variabile < 1){
+//         return alert("devi inserire un numero compreso tra 1 e 5") 
+//     }
+// }
+
+function isInRange(number){
+    if (number > 5 || number < 1){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// function isInRangeBetween(number, min, max){
+//     if (number > max || number < min){
+//         return false 
+//     }else{
+//         return true;
+//     }
+// }
